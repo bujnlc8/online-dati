0cafc45eee6a1740c07b53eeca195460f7d572e1
凌海辉 <linghaihui@licaigc.com>
2018-02-06 12:10:56 +0800

    get inet address automatic

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -6,5 +6,5 @@ do
         export ${line//\'/}
     fi
 done
-
-gunicorn -k gevent -w 1 -b 192.168.1.9:8888  app:App
+inet=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:" | awk 'NR==1'`
+gunicorn -k gevent -w 1 -b ${inet}:8888  app:App
